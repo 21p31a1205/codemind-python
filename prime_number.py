3d8f70716d135a4a437e28261f80aae5d0ae8070
21p31a1205 <105282388+21p31a1205@users.noreply.github.com>
2022-10-15 07:50:39 +0530

    n=int(input()) flag=0 for i in range(2,n):     if n%i==0:         flag=1         break if flag==0:     print("prime") else:     print("not a prime")

--- prime_number.py
+++ prime_number.py
@@ -1,13 +1,10 @@
-def prime(m):
-    if m<=2:
-        return 0
-    for i in range(2,int(m**0.5)+1):
-        if (m%i==0):
-            return 0
-    return 1
 n=int(input())
-if prime(n):
+flag=0
+for i in range(2,n):
+    if n%i==0:
+        flag=1
+        break
+if flag==0:
     print("prime")
 else:
     print("not a prime")
-    

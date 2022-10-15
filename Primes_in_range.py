ab134b0fca7cae6b80591ae399c251b61b58ec47
21p31a1205 <105282388+21p31a1205@users.noreply.github.com>
2022-10-15 07:52:22 +0530

    def isPrime(i):     if i==1:         return False     for j in range(2,int(i**0.5)+1):         if(i%j==0):             return False     return True
    c=0
    n=int(input())
    m=int(input())
    for i in range(n,m+1):
        if(isPrime(i)):
            c+=1
    print(c)

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,13 +1,15 @@
-import math
-def prime(a):
- for i in range(2,round(math.sqrt(a))+1):
-   if a%i==0:
-      return 0
- return 1
-a=int(input())
-b=int(input())
+def isPrime(i):
+    if i==1:
+        return False
+    for j in range(2,int(i**0.5)+1):
+        if(i%j==0):
+            return False
+    return True
+
 c=0
-for i in range(a,b+1):
-  if prime(i)==1 and i!=1:
-     c+=1
+n=int(input())
+m=int(input())
+for i in range(n,m+1):
+    if(isPrime(i)):
+        c+=1
 print(c)

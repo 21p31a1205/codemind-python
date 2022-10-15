d8a9e0eb68ee216578c0eb3ada4d015be16ead09
21p31a1205 <105282388+21p31a1205@users.noreply.github.com>
2022-10-15 06:51:31 +0530

    n=int(input()) m=int(input()) for i in range(n,m+1):     if "0" not in str(i):         flag=1         for j in str(i):             if(i%int(j)!=0):                 flag=0         if(flag==1):             print(i,end=" ")

--- Self_Dividing_Numbers.py
+++ Self_Dividing_Numbers.py
@@ -1,20 +1,10 @@
-a=int(input())
-b=int(input())
-for i in range(a,b+1) :
-    v=i
-    j=i
-    s=0
-    count=0
-    while(i>0) :
-      s=i%10
-      count=count+1
-      i=i//10
-    c=0
-    while j>0 :
-        r=j%10
-        if (r!=0 and v%r==0 ) :
-            c=c+1
-        j=j//10
-    if(count==c) :
-        print(v,end=" ")
-        
+n=int(input())
+m=int(input())
+for i in range(n,m+1):
+    if "0" not in str(i):
+        flag=1
+        for j in str(i):
+            if(i%int(j)!=0):
+                flag=0
+        if(flag==1):
+            print(i,end=" ")
